f6540671a2ef216a457dcdcb405c082257453ef2
Augusto6742 <augusto.dovirgens@escola.pr.gov.br>
2026-08-30 19:28:04 -0300

    a

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// Base de dados expandida com 20 receitas
+// Base de dados com 20 receitas
 const receitas = [
-  // --- ITALIANA (5 receitas) ---
+  // --- ITALIANA ---
   {
     id: 1,
     nome: "Spaghetti alla Carbonara",
@@ -38,12 +38,12 @@ const receitas = [
     cultura: "italiana",
     imagem: "https://images.unsplash.com/photo-1571877227200-a0d98ea607e9?auto=format&fit=crop&w=600&q=80",
     videoUrl: "https://www.youtube.com/embed/p9YxK-aF5x8",
-    ingredientes: ["500g de queijo Mascarpone", "4 ovos (gemas e claras separadas)", "100g de açúcar", "200g de biscoito Champagne", "1 xícara de café forte sem açúcar", "Cacau em pó para polvilhar"],
+    ingredientes: ["500g de queijo Mascarpone", "4 ovos", "100g de açúcar", "200g de biscoito Champagne", "1 xícara de café forte sem açúcar", "Cacau em pó"],
     instrucoes: [
-      "Bata as gemas com o açúcar até obter um creme claro e adicione o mascarpone.",
-      "Bata as claras em neve e incorpore suavemente ao creme.",
-      "Passe os biscoitos rapidamente pelo café frio.",
-      "Monte camadas de biscoito e creme, finalizando com cacau em pó. Leve à geladeira."
+      "Bata as gemas com o açúcar e incorpore o mascarpone.",
+      "Bata as claras em neve e adicione ao creme suavemente.",
+      "Passe os biscoitos no café frio.",
+      "Monte camadas de biscoito e creme, finalizando com cacau em pó."
     ]
   },
   {
@@ -56,8 +56,8 @@ const receitas = [
     ingredientes: ["300g de arroz Arbóreo", "200g de cogumelos variados", "1 litro de caldo de legumes", "1/2 xícara de vinho branco", "100g de queijo parmesão", "2 colheres de manteiga"],
     instrucoes: [
       "Refogue os cogumelos na manteiga e reserve.",
-      "Na mesma panela, refogue o arroz, adicione o vinho e deixe evaporar.",
-      "Vá adicionando o caldo quente aos poucos, mexendo sempre.",
+      "Refogue o arroz, adicione o vinho e deixe evaporar.",
+      "Adicione o caldo quente aos poucos, mexendo sempre.",
       "Quando o arroz estiver al dente, misture os cogumelos, a manteiga e o parmesão."
     ]
   },
@@ -68,16 +68,16 @@ const receitas = [
     cultura: "italiana",
     imagem: "https://images.unsplash.com/photo-1551024709-8f23befc6f87?auto=format&fit=crop&w=600&q=80",
     videoUrl: "https://www.youtube.com/embed/O49vIuPnp_c",
-    ingredientes: ["Massas para cannoli", "500g de Ricota fresca", "150g de açúcar de confeiteiro", "Gotas de chocolate amargo", "Pistasgache picado para decorar"],
+    ingredientes: ["Massas para cannoli", "500g de Ricota fresca", "150g de açúcar de confeiteiro", "Gotas de chocolate amargo", "Pistache picado"],
     instrucoes: [
-      "Passe a ricota por uma peneira para ficar bem cremosa.",
+      "Passe a ricota por uma peneira para ficar cremosa.",
       "Misture a ricota com o açúcar e as gotas de chocolate.",
       "Frite os tubos de massa até dourarem.",
-      "Recheie os tubos com o creme apenas no momento de servir e decore com pistache."
+      "Recheie os tubos com o creme e decore com pistache."
     ]
   },
 
-  // --- JAPONESA (5 receitas) ---
+  // --- JAPONESA ---
   {
     id: 6,
     nome: "Ramen Shoyu Traditional",
@@ -85,11 +85,11 @@ const receitas = [
     cultura: "japonesa",
     imagem: "https://images.unsplash.com/photo-1569718212165-3a8278d5f624?auto=format&fit=crop&w=600&q=80",
     videoUrl: "https://www.youtube.com/embed/B8y3BHt1Xn0",
-    ingredientes: ["Macarrão para ramen", "Caldo de galinha e porco encorpado", "Shoyu a gosto", "Chashu (fatias de barriga de porco)", "Ovo marinado (Ajitsuke Tamago)", "Cebolinha picada"],
+    ingredientes: ["Macarrão para ramen", "Caldo encorpado", "Shoyu a gosto", "Chashu (barriga de porco)", "Ovo marinado", "Cebolinha"],
     instrucoes: [
       "Prepare o caldo bem quente temperado com a base de shoyu.",
-      "Cozinhe o macarrão de ramen até ficar al dente.",
-      "Coloque o macarrão na tigela e cubra com o caldo escaldante.",
+      "Cozinhe o macarrão até ficar al dente.",
+      "Coloque o macarrão na tigela e cubra com o caldo.",
       "Decore com o porco, o ovo cortado ao meio e cebolinha."
     ]
   },
@@ -100,10 +100,10 @@ const receitas = [
     cultura: "japonesa",
     imagem: "https://images.unsplash.com/photo-1563805042-7684c019e1cb?auto=format&fit=crop&w=600&q=80",
     videoUrl: "https://www.youtube.com/embed/5H-7Z4eDGE8",
-    ingredientes: ["2 ovos", "50g de açúcar", "1 colher de mel", "100g de farinha de trigo", "Anko (pasta de feijão doce azuki)"],
+    ingredientes: ["2 ovos", "50g de açúcar", "1 colher de mel", "100g de farinha de trigo", "Anko (pasta de feijão doce)"],
     instrucoes: [
       "Bata os ovos, açúcar e mel até espumar.",
-      "Peneire a farinha e adicione um pouco de água para dar o ponto de massa.",
+      "Peneire a farinha e adicione um pouco de água.",
       "Cozinhe discos de massa em uma frigideira antiaderente.",
       "Faça um sanduíche com duas panquecas recheadas com anko."
     ]
@@ -115,26 +115,25 @@ const receitas = [
     cultura: "japonesa",
     imagem: "https://images.unsplash.com/photo-1603894584373-5ac82b2ae398?auto=format&fit=crop&w=600&q=80",
     videoUrl: "https://www.youtube.com/embed/55hS05r10_4",
-    ingredientes: ["2 filés de peito de frango", "Farinha Panko", "1 tablete de Curry japonês", "Batata e cenoura em cubos", "Arroz japonês (Gohan)"],
+    ingredientes: ["2 filés de frango", "Farinha Panko", "1 tablete de Curry japonês", "Batata e cenoura", "Arroz japonês"],
     instrucoes: [
-      "Empane o frango na farinha de trigo, ovo e panko, e frite até dourar.",
-      "Cozinhe os legumes e dissolva o tablete de curry para fazer o molho encorpado.",
-      "Sirva o frango fatiado ao lado do arroz japonês e cubra com o molho de curry."
+      "Empane o frango na farinha de trigo, ovo e panko, e frite.",
+      "Cozinhe os legumes e dissolva o curry para o molho.",
+      "Sirva o frango fatiado ao lado do arroz e cubra com o molho."
     ]
   },
   {
     id: 9,
-    nome: "Mochi de Morango (Ichigo Daifuku)",
+    nome: "Mochi de Morango",
     tipo: "doce",
     cultura: "japonesa",
     imagem: "https://images.unsplash.com/photo-1582293041079-7814c2f12063?auto=format&fit=crop&w=600&q=80",
     videoUrl: "https://www.youtube.com/embed/9G_0l2V4f4A",
-    ingredientes: ["100g de farinha de arroz glutinoso (Shiratamako)", "180ml de água", "50g de açúcar", "Morangos frescos", "Anko (pasta doce de feijão)"],
+    ingredientes: ["100g de farinha de arroz glutinoso", "180ml de água", "50g de açúcar", "Morangos frescos", "Anko"],
     instrucoes: [
-      "Envolva os morangos em uma camada fina de pasta de feijão anko.",
+      "Envolva os morangos em uma camada fina de anko.",
       "Misture a farinha de arroz, água e açúcar, e leve ao micro-ondas até formar uma massa elástica.",
-      "Polvilhe amido de milho nas mãos e abra pequenas porções de massa.",
-      "Envolva o morango preparado com a massa de mochi."
+      "Abra pequenas porções de massa e envolva o morango."
     ]
   },
   {
@@ -144,16 +143,16 @@ const receitas = [
     cultura: "japonesa",
     imagem: "https://images.unsplash.com/photo-1496116218417-1a781b1c416c?auto=format&fit=crop&w=600&q=80",
     videoUrl: "https://www.youtube.com/embed/rV3i6U-tXW8",
-    ingredientes: ["Massas para gyoza", "300g de carne suína moída", "Acelga bem picada", "Gengibre e alho ralados", "Óleo de gergelim e shoyu"],
+    ingredientes: ["Massas para gyoza", "300g de carne suína moída", "Acelga picada", "Gengibre e alho", "Óleo de gergelim"],
     instrucoes: [
-      "Misture a carne, acelga, gengibre, alho e temperos até formar um recheio homogêneo.",
-      "Recheie as massas e feche fazendo pequenas dobras na borda.",
-      "Frite o fundo dos gyozas em uma frigideira com óleo de gergelim.",
-      "Adicione um pouco de água, tape a frigideira e deixe cozinhar no vapor."
+      "Misture a carne, acelga e temperos para o recheio.",
+      "Recheie as massas e dobre as bordas.",
+      "Frite o fundo dos gyozas em uma frigideira.",
+      "Adicione um pouco de água, tampe e deixe cozinhar no vapor."
     ]
   },
 
-  // --- MEXICANA (5 receitas) ---
+  // --- MEXICANA ---
   {
     id: 11,
     nome: "Tacos de Carne Asada",
@@ -163,10 +162,10 @@ const receitas = [
     videoUrl: "https://www.youtube.com/embed/v8l5wI7aK8Y",
     ingredientes: ["500g de fraldinha", "Tortilhas de milho", "Coentro picado", "Cebola picada", "Limão e sal"],
     instrucoes: [
-      "Tempere a carne e grelhe em fogo bem alto.",
+      "Tempere a carne e grelhe em fogo alto.",
       "Corte a carne em cubos bem pequenos.",
       "Aqueça as tortilhas na chapa.",
-      "Monte os tacos com carne, cebola, coentro e finalize com gotas de limão."
+      "Monte os tacos com carne, cebola, coentro e limão."
     ]
   },
   {
@@ -176,12 +175,12 @@ const receitas = [
     cultura: "mexicana",
     imagem: "https://images.unsplash.com/photo-1624371414361-e670ef4889d6?auto=format&fit=crop&w=600&q=80",
     videoUrl: "https://www.youtube.com/embed/6S2n4Q7dE6g",
-    ingredientes: ["250ml de água", "2 colheres de manteiga", "2 colheres de açúcar", "150g de farinha de trigo", "Açúcar e canela para polvilhar", "Doce de leite para acompanhar"],
+    ingredientes: ["250ml de água", "2 colheres de manteiga", "2 colheres de açúcar", "150g de farinha de trigo", "Açúcar e canela", "Doce de leite"],
     instrucoes: [
-      "Ferva a água com a manteiga e o açúcar.",
-      "Adicione a farinha de uma vez e mexa até soltar do fundo da panela.",
-      "Coloque a massa em um saco de confeitar com bico pitanga e modele os churros.",
-      "Frite em óleo quente, passe na canela com açúcar e sirva com doce de leite."
+      "Ferva a água com manteiga e açúcar.",
+      "Adicione a farinha e mexa até soltar da panela.",
+      "Modele os churros com bico pitanga.",
+      "Frite em óleo quente, passe na canela e sirva com doce de leite."
     ]
   },
   {
@@ -191,12 +190,12 @@ const receitas = [
     cultura: "mexicana",
     imagem: "https://images.unsplash.com/photo-1540420773420-3366772f4999?auto=format&fit=crop&w=600&q=80",
     videoUrl: "https://www.youtube.com/embed/3KkOzA_v_v0",
-    ingredientes: ["2 abacates maduros (ou avocados)", "1 tomate sem semente em cubos", "1/2 cebola roxa picada", "Suco de 1 limão", "Coentro fresco", "Tortilhas de milho fritas (Totopos)"],
+    ingredientes: ["2 abacates maduros", "1 tomate em cubos", "1/2 cebola roxa picada", "Suco de 1 limão", "Coentro fresco", "Totopos"],
     instrucoes: [
-      "Amasse o abacate com um garfo deixando alguns pedaços.",
-      "Adicione o tomate, cebola roxa e coentro picado.",
-      "Tempere com o suco de limão e sal a gosto.",
-      "Sirva imediatamente acompanhado de totopos bem crocantes."
+      "Amasse o abacate deixando alguns pedaços.",
+      "Adicione tomate, cebola e coentro.",
+      "Tempere com limão e sal.",
+      "Sirva acompanhado de totopos."
     ]
   },
   {
@@ -206,12 +205,12 @@ const receitas = [
     cultura: "mexicana",
     imagem: "https://images.unsplash.com/photo-1464349095431-e9a21285b5f3?auto=format&fit=crop&w=600&q=80",
     videoUrl: "https://www.youtube.com/embed/Q4X-P6vWz6E",
-    ingredientes: ["Pão de ló básico", "1 lata de leite condensado", "1 lata de leite evaporado", "1 xícara de creme de leite fresco", "Chantilly e canela para decorar"],
+    ingredientes: ["Pão de ló básico", "1 lata de leite condensado", "1 lata de leite evaporado", "1 xícara de creme de leite", "Chantilly"],
     instrucoes: [
-      "Asse o pão de ló e deixe esfriar completamente.",
-      "Misture os três leites (condensado, evaporado e creme de leite).",
-      "Fure todo o bolo com um garfo e despeje a mistura de leites lentamente.",
-      "Cubra com chantilly e leve à geladeira antes de servir."
+      "Asse o pão de ló e deixe esfriar.",
+      "Misture os três leites.",
+      "Fure o bolo com um garfo e despeje a mistura.",
+      "Cubra com chantilly e leve à geladeira."
     ]
   },
   {
@@ -221,16 +220,16 @@ const receitas = [
     cultura: "mexicana",
     imagem: "https://images.unsplash.com/photo-1534352956036-cd81e27dd615?auto=format&fit=crop&w=600&q=80",
     videoUrl: "https://www.youtube.com/embed/fA3f5k4Cq5E",
-    ingredientes: ["Tortilhas de milho", "400g de frango desfiado temperado", "Molho de pimenta vermelha para enchilada", "200g de queijo cheddar ou prato ralado", "Creme azedo (sour cream)"],
+    ingredientes: ["Tortilhas de milho", "400g de frango desfiado", "Molho de pimenta vermelha", "200g de queijo ralado", "Creme azedo"],
     instrucoes: [
-      "Passe as tortilhas ligeiramente no molho quente.",
-      "Recheie com o frango desfiado e enrole como panquecas.",
-      "Disponha em um refratário, cubra com mais molho e bastante queijo.",
-      "Leve ao forno para gratinar e sirva com creme azedo."
+      "Passe as tortilhas no molho quente.",
+      "Recheie com frango e enrole.",
+      "Disponha num refratário, cubra com molho e queijo.",
+      "Gratine no forno e sirva."
     ]
   },
 
-  // --- BRASILEIRA (5 receitas) ---
+  // --- BRASILEIRA ---
   {
     id: 16,
     nome: "Brigadeiro Gourmet",
@@ -238,11 +237,11 @@ const receitas = [
     cultura: "brasileira",
     imagem: "https://images.unsplash.com/photo-1541781774459-bb2af2f05b55?auto=format&fit=crop&w=600&q=80",
     videoUrl: "https://www.youtube.com/embed/L1M4k6iN4m8",
-    ingredientes: ["1 lata de leite condensado", "200g de creme de leite", "100g de chocolate nobre 50%", "Granulado de qualidade"],
+    ingredientes: ["1 lata de leite condensado", "200g de creme de leite", "100g de chocolate 50%", "Granulado"],
     instrucoes: [
-      "Junte todos os ingredientes em uma panela de fundo grosso.",
-      "Cozinhe em fogo médio/baixo sem parar de mexer até desprender do fundo.",
-      "Deixe esfriar completamente, modele as bolinhas e passe no granulado."
+      "Junte todos os ingredientes na panela.",
+      "Cozinhe em fogo baixo até desprender do fundo.",
+      "Deixe esfriar, modele as bolinhas e passe no granulado."
     ]
   },
   {
@@ -252,67 +251,69 @@ const receitas = [
     cultura: "brasileira",
     imagem: "https://images.unsplash.com/photo-1567620832903-9fc6debc209f?auto=format&fit=crop&w=600&q=80",
     videoUrl: "https://www.youtube.com/embed/5aK0B7xJ2U8",
-    ingredientes: ["500g de feijão preto", "300g de carne seca", "200g de lombo defumado", "2 linguiças calabresas", "Paiol, alho, cebola e folhas de louro"],
+    ingredientes: ["500g de feijão preto", "300g de carne seca", "200g de lombo defumado", "2 linguiças calabresas", "Alho, cebola e louro"],
     instrucoes: [
-      "Dessalgue as carnes secas por 24 horas.",
-      "Cozinhe o feijão com as carnes mais duras na pressão.",
+      "Dessalgue as carnes secas.",
+      "Cozinhe o feijão com as carnes mais duras.",
       "Adicione as linguiças e termine o cozimento.",
-      "Refogue alho e cebola e junte ao feijão para engrossar o caldo."
+      "Refogue alho e cebola e junte ao feijão."
     ]
   },
   {
     id: 18,
-    nome: "Pudim de Leite Condensado",
+    nome: "Pudim de Leite",
     tipo: "doce",
     cultura: "brasileira",
     imagem: "https://images.unsplash.com/photo-1528975604071-b4dc52a2d18c?auto=format&fit=crop&w=600&q=80",
     videoUrl: "https://www.youtube.com/embed/9Z9P9_nQ1U8",
-    ingredientes: ["1 lata de leite condensado", "1 lata de leite (mesma medida)", "3 ovos inteiros", "1 xícara de açúcar para a calda"],
+    ingredientes: ["1 lata de leite condensado", "1 lata de leite", "3 ovos", "1 xícara de açúcar"],
     instrucoes: [
-      "Derreta o açúcar na forma de pudim até virar um caramelo dourado.",
-      "Bata o leite condensado, o leite e os ovos no liquidificador por 3 minutos.",
-      "Despeje a mistura na forma caramelizada.",
-      "Asse em banho-maria no forno pré-aquecido a 180°C por cerca de 1 hora."
+      "Derreta o açúcar na forma de pudim.",
+      "Bata o leite condensado, leite e ovos no liquidificador.",
+      "Despeje na forma caramelizada.",
+      "Asse em banho-maria por 1 hora."
     ]
   },
   {
     id: 19,
-    nome: "Moqueca Baiana de Peixe",
+    nome: "Moqueca Baiana",
     tipo: "salgado",
     cultura: "brasileira",
     imagem: "https://images.unsplash.com/photo-1534422298391-e4f8c172dddb?auto=format&fit=crop&w=600&q=80",
     videoUrl: "https://www.youtube.com/embed/8vR4M_W2U8c",
-    ingredientes: ["800g de postas de peixe (Robalo ou Cação)", "200ml de leite de coco", "3 colheres de azeite de dendê", "Pimentões coloridos e cebola em rodelas", "Coentro picado"],
+    ingredientes: ["800g de postas de peixe", "200ml de leite de coco", "3 colheres de azeite de dendê", "Pimentões e cebola", "Coentro"],
     instrucoes: [
-      "Marine o peixe com limão, alho e sal por 30 minutos.",
-      "Monte em uma panela de barro camadas de cebola, pimentão e peixe.",
-      "Regue com o leite de coco e o azeite de dendê.",
-      "Cozinhe em fogo baixo com a panela tampada por 20 minutos e finalize com coentro."
+      "Marine o peixe com limão, alho e sal.",
+      "Monte camadas de vegetais e peixe na panela.",
+      "Regue com leite de coco e dendê.",
+      "Cozinhe em fogo baixo por 20 minutos."
     ]
   },
   {
     id: 20,
-    nome: "Coxinha de Frango com Catupiry",
+    nome: "Coxinha de Frango",
     tipo: "salgado",
     cultura: "brasileira",
     imagem: "https://images.unsplash.com/photo-1569058242567-93de6f36f8e6?auto=format&fit=crop&w=600&q=80",
     videoUrl: "https://www.youtube.com/embed/3AAdKl1UYZs",
-    ingredientes: ["500ml de caldo de frango", "300g de farinha de trigo", "50g de manteiga", "300g de frango desfiado temperado", "Requeijão tipo Catupiry", "Farinha de rosca e ovos para empanar"],
+    ingredientes: ["500ml de caldo de frango", "300g de farinha de trigo", "50g de manteiga", "300g de frango desfiado", "Catupiry"],
     instrucoes: [
-      "Ferva o caldo com a manteiga, adicione a farinha e mexa vigorosamente até cozinhar a massa.",
-      "Deixe a massa esfriar e abra pequenas porções na mão.",
-      "Recheie com o frango e uma colher de catupiry.",
-      "Modele no formato de coxinha, empane no ovo e farinha de rosca e frite em óleo quente."
+      "Ferva o caldo com manteiga e misture a farinha até dar o ponto de massa.",
+      "Abra a massa, recheie com frango e catupiry.",
+      "Modele no formato de coxinha.",
+      "Empane no ovo e farinha de rosca e frite."
     ]
   }
-  // Elementos da DOM
+];
+
+// Mapeamento dos elementos da página
 const recipeGrid = document.getElementById('recipeGrid');
 const selectTipo = document.getElementById('tipo');
 const selectCultura = document.getElementById('cultura');
 const modal = document.getElementById('recipeModal');
 const closeBtn = document.getElementById('closeBtn');
 
-// Renderização dos cartões
+// Renderização dos cards
 function renderizarReceitas() {
   const tipoFiltro = selectTipo.value;
   const culturaFiltro = selectCultura.value;
@@ -349,7 +350,7 @@ function renderizarReceitas() {
   });
 }
 
-// Abrir Modal
+// Controle do Modal
 function abrirModal(receita) {
   const modalTitle = document.getElementById('modalTitle');
   modalTitle.innerText = receita.nome;
@@ -370,13 +371,12 @@ function abrirModal(receita) {
   modal.style.display = 'flex';
 }
 
-// Fechar Modal
-closeBtn.onclick = fecharModal;
-
 function fecharModal() {
   modal.style.display = 'none';
   document.getElementById('modalVideo').src = '';
 }
+
+closeBtn.onclick = fecharModal;
 
 window.onclick = (event) => {
   if (event.target === modal) {
@@ -384,10 +384,9 @@ window.onclick = (event) => {
   }
 };
 
-// Eventos dos seletores
+// Eventos de mudança nos filtros
 selectTipo.addEventListener('change', renderizarReceitas);
 selectCultura.addEventListener('change', renderizarReceitas);
 
-// Inicialização (FAZ AS RECEITAS APARECEREM)
+// Executa a renderização assim que o script carregar
 renderizarReceitas();
-];
